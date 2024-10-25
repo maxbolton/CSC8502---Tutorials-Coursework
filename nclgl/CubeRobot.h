b07a3419bcdfe01cf48560b8968e99e8037e9836
maxbolton <maxwell.bolton@mail.com>
2024-10-25 13:05:14 +0100

    Add Scene Graph and Renderer implementation
    - Tutorial6.vcxproj: Added Renderer.cpp and Renderer.h to the project.
    - Tutorial6.vcxproj.filters: Added Renderer.cpp to ClCompile, created filter for Header Files, and added Renderer.h.
    - GraphicsTutorials.sln: Added project dependencies for "Tutorial 06 - Scene Graph" to depend on "Tutorial 05 - Scissors".
    - nclgl.vcxproj: Added CubeRobot.cpp and SceneNode.cpp to ClCompile, and CubeRobot.h and SceneNode.h to ClInclude.
    - nclgl.vcxproj.filters: Added SceneNode.cpp and CubeRobot.cpp to ClCompile, and SceneNode.h and CubeRobot.h to ClInclude.
    - CubeRobot.cpp: Implemented CubeRobot class for constructing and updating a robot using SceneNode objects.
    - CubeRobot.h: Declared CubeRobot class with methods for construction and updating.
    - Renderer.cpp: Implemented Renderer class for setting up and rendering the scene, including a CubeRobot.
    - Renderer.h: Declared Renderer class with methods for updating and rendering the scene.
    - SceneNode.cpp: Implemented SceneNode class for scene graph nodes, with methods for adding children, updating transformations, and drawing.
    - SceneNode.h: Declared SceneNode class with properties and methods for transformation, color, mesh, and children nodes.
    - SceneFragment.glsl: Added fragment shader for coloring and optional texturing of scene nodes.
    - SceneVertex.glsl: Added vertex shader for transforming vertex positions and passing texture coordinates and colors to the fragment shader.

---
+++ nclgl/CubeRobot.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "..\nclgl\SceneNode.h"
+
+class CubeRobot : public SceneNode
+{
+public:
+	CubeRobot(Mesh* cube);
+	~CubeRobot(void) {};
+	void Update(float dt) override;
+
+protected:
+	SceneNode* body;
+	SceneNode* head;
+	SceneNode* leftArm;
+	SceneNode* rightArm;
+	SceneNode* leftLeg;
+	SceneNode* rightLeg;
+
+};
+
